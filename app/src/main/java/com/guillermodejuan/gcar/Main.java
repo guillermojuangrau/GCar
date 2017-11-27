3623095d60ef293abb5c9280173078eb4160cdc7
guillermojuangrau <guillermo@guillermodejuan.com>
2017-11-27 19:38:19 +0000

    Created settings screen and make it work to read address and go back to main activity with result.

--- app/src/main/java/com/guillermodejuan/gcar/Main.java
+++ app/src/main/java/com/guillermodejuan/gcar/Main.java
@@ -1,7 +1,9 @@
 package com.guillermodejuan.gcar;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.Intent;
 import android.location.LocationManager;
 import android.media.AudioManager;
 import android.support.v7.app.ActionBar;
@@ -18,27 +20,6 @@ import android.widget.Toast;
  * status bar and navigation/system bar) with user interaction.
  */
 public class Main extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-
-    private View mControlsView;
 
     ImageButton bluetoothButton;
     ImageButton dataButton;
@@ -58,6 +39,7 @@ public class Main extends AppCompatActivity {
     boolean dataenabled;
     int volume;
     int maxvolume;
+    String navigationaddress = "Default address";
 
 
     BluetoothAdapter mBluetoothAdapter;
@@ -69,14 +51,7 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mContentView = findViewById(R.id.fullscreen_content);
-//        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        //Hides the status and the title bar
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
@@ -176,24 +151,28 @@ public class Main extends AppCompatActivity {
 
     }
     private void linkButtonsWithTheirViews(){
-        bluetoothButton = (ImageButton) findViewById(R.id.bluetooth);
-        dataButton = (ImageButton) findViewById(R.id.data);
-        gpsButton = (ImageButton) findViewById(R.id.gps);
-        volumeButton = (ImageButton) findViewById(R.id.volume);
-        settingsButton = (ImageButton) findViewById(R.id.settings);
-        callanaButton = (ImageButton) findViewById(R.id.callana);
-        rebootButton = (ImageButton) findViewById(R.id.reboot);
-        torqueButton = (ImageButton) findViewById(R.id.torque);
-        musicButton = (ImageButton) findViewById(R.id.music);
-        mapsButton = (ImageButton) findViewById(R.id.maps);
-        homeButton = (ImageButton) findViewById(R.id.homebutton);
-        gButton = (ImageButton) findViewById(R.id.gbutton);
+        bluetoothButton =   findViewById(R.id.bluetooth);
+        dataButton      =   findViewById(R.id.data);
+        gpsButton       =   findViewById(R.id.gps);
+        volumeButton    =   findViewById(R.id.volume);
+        settingsButton  =   findViewById(R.id.settings);
+        callanaButton   =   findViewById(R.id.callana);
+        rebootButton    =   findViewById(R.id.reboot);
+        torqueButton    =   findViewById(R.id.torque);
+        musicButton     =   findViewById(R.id.music);
+        mapsButton      =   findViewById(R.id.maps);
+        homeButton      =   findViewById(R.id.homebutton);
+        gButton         =   findViewById(R.id.gbutton);
         return;
     }
 
     private void showSettingsPopup(){
-        //TODO
+        Intent i = new Intent(this, SettingsActivity.class);
+        i.putExtra("address", navigationaddress);
+        startActivityForResult(i,1);
     }
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -201,12 +180,22 @@ public class Main extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                navigationaddress = data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }
+
 private View.OnClickListener buttonListener = new View.OnClickListener() {
         public void onClick(View v) {
-            // do something when the button is clicked
-            // Yes we will handle click here but which button clicked??? We don't know
 
-            // So we will make
             switch (v.getId() /*to get clicked view id**/) {
                 case R.id.bluetooth:
 
@@ -228,14 +217,8 @@ private View.OnClickListener buttonListener = new View.OnClickListener() {
                     break;
                 case R.id.gps:
 
-                     Toast.makeText(Main.this,
-                "gps is clicked!", Toast.LENGTH_SHORT).show();
-
                     break;
                 case R.id.data:
-
-                    Toast.makeText(Main.this,
-                "data is clicked!", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.volume:
 
--- app/src/main/java/com/guillermodejuan/gcar/SettingsActivity.java
+++ app/src/main/java/com/guillermodejuan/gcar/SettingsActivity.java
@@ -1,50 +1,22 @@
 package com.guillermodejuan.gcar;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class SettingsActivity extends AppCompatActivity {
 
-
-    // UI references.
-
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+Button acceptButton;
+TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +24,8 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
 
-        Button acceptButton = (Button) findViewById(R.id.acceptsettingsbutton);
+        acceptButton = findViewById(R.id.accept);
+        text =  findViewById(R.id.address);
         acceptButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,6 +37,10 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     private void recordDataAndGoBack() {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("result", text.getEditableText());
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
 
     }
 
