f2587c3ff25c358948988bd5f00f31aba75c552e
SPECTRUM\guillermo.juan <guillermo.juan@spectrummedical.co.uk>
2017-11-27 11:16:57 +0000

    Added functionality to test buttons are working. Started drafting methods for later and adding the TODO label.

--- app/src/main/java/com/guillermodejuan/gcar/Main.java
+++ app/src/main/java/com/guillermodejuan/gcar/Main.java
@@ -1,12 +1,15 @@
 package com.guillermodejuan.gcar;
 
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -35,6 +38,20 @@ public class Main extends AppCompatActivity {
 
     private View mControlsView;
 
+    ImageButton bluetoothButton;
+    ImageButton dataButton;
+    ImageButton gpsButton;
+    ImageButton volumeButton;
+    ImageButton settingsButton;
+    ImageButton callanaButton;
+    ImageButton rebootButton;
+    ImageButton torqueButton;
+    ImageButton musicButton;
+    ImageButton mapsButton;
+    ImageButton homeButton;
+    ImageButton gButton;
+    boolean gbuttonState = true;
+    BluetoothAdapter mBluetoothAdapter;
 
 
     @Override
@@ -55,7 +72,54 @@ public class Main extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        linkButtonsWithTheirViews();
+        addButtonListeners();
+        setUpAdapters();
+        setButtonStates();
+
     }
+
+    //Method to set up all the adapters to the phone's functionality
+    private void setUpAdapters(){
+        //TODO
+        return;
+    }
+    //Method to set the initial button states
+    private void setButtonStates(){
+        //TODO
+        return;
+    }
+    private void addButtonListeners(){
+        bluetoothButton.setOnClickListener(buttonListener);
+        dataButton.setOnClickListener(buttonListener);
+        gpsButton.setOnClickListener(buttonListener);
+        volumeButton.setOnClickListener(buttonListener);
+        settingsButton.setOnClickListener(buttonListener);
+        callanaButton.setOnClickListener(buttonListener);
+        rebootButton.setOnClickListener(buttonListener);
+        torqueButton.setOnClickListener(buttonListener);
+        musicButton.setOnClickListener(buttonListener);
+        mapsButton.setOnClickListener(buttonListener);
+        homeButton.setOnClickListener(buttonListener);
+        gButton.setOnClickListener(buttonListener);
+
+    }
+    private void linkButtonsWithTheirViews(){
+        bluetoothButton = (ImageButton) findViewById(R.id.bluetooth);
+        dataButton = (ImageButton) findViewById(R.id.data);
+        gpsButton = (ImageButton) findViewById(R.id.gps);
+        volumeButton = (ImageButton) findViewById(R.id.volume);
+        settingsButton = (ImageButton) findViewById(R.id.settings);
+        callanaButton = (ImageButton) findViewById(R.id.callana);
+        rebootButton = (ImageButton) findViewById(R.id.reboot);
+        torqueButton = (ImageButton) findViewById(R.id.torque);
+        musicButton = (ImageButton) findViewById(R.id.music);
+        mapsButton = (ImageButton) findViewById(R.id.maps);
+        homeButton = (ImageButton) findViewById(R.id.homebutton);
+        gButton = (ImageButton) findViewById(R.id.gbutton);
+        return;
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -63,7 +127,90 @@ public class Main extends AppCompatActivity {
 
     }
 
+private View.OnClickListener buttonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            // Yes we will handle click here but which button clicked??? We don't know
 
+            // So we will make
+            switch (v.getId() /*to get clicked view id**/) {
+                case R.id.bluetooth:
+
+                     Toast.makeText(Main.this,
+                "bluetooth is clicked!", Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.gps:
+
+                     Toast.makeText(Main.this,
+                "gps is clicked!", Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.data:
+
+                    Toast.makeText(Main.this,
+                "data is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.volume:
+
+                    Toast.makeText(Main.this,
+                "volume is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.callana:
+
+                    Toast.makeText(Main.this,
+                "callana is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.settings:
+
+                    Toast.makeText(Main.this,
+                "settings is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.reboot:
+
+                    Toast.makeText(Main.this,
+                            "reboot is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.torque:
+
+                    Toast.makeText(Main.this,
+                "torque is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.maps:
+
+                     Toast.makeText(Main.this,
+                "maps is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.music:
+
+                    Toast.makeText(Main.this,
+                "music is clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.homebutton:
+
+                     Toast.makeText(Main.this,
+                "home is clicked!", Toast.LENGTH_SHORT).show();
+
+                    break;
+
+                case R.id.gbutton:
+                    if(gbuttonState){
+                        gButton.setImageResource(R.drawable.centrebuttonred_round);
+                        gbuttonState=false;
+                    }else{
+                        gButton.setImageResource(R.drawable.centrebuttongreen_round);
+                        gbuttonState=true;
+                    }
+
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
 
 }
